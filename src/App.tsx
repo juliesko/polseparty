408923c6d88528d056750de9e6febd44a5be5051
Gaute Solbu Kleiven <gaute@Gautes-MacBook-Pro.local>
2021-10-10 19:11:09 +0200

    Oppdater med lag og ny agenda

--- src/App.tsx
+++ src/App.tsx
@@ -43,16 +43,80 @@ function App() {
         </div>
         <div className="innholdsboks">
           <h3>Agenda</h3>
-            <p>Live, love, lounge med velkomstbobler i labben</p>
-            <p>"Rebus"</p>
-            <p>Pils, pøls og premiering - KJØH</p>
-            <p>Pandoras</p>
-            <p>Klisjé-Kjells Karaokeparty </p>
-            <p>Natta eller ikke natta ;-)</p>
+            <p>16:00 - 17:00: Mottakelse og velkomstbobler</p>
+            <p>17:00 - 18:30: "Rebus"</p>
+            <p>19:00: PØLSER!!!!!!!!!!</p>
+            <br/>
+            <p>Resten av kvelden:</p>
+            <p>Fri lek</p>
+            <p>Kanskje kaste plastballer oppi noen kopper</p>
+            <p>Digge grooves fra DJ Bergman</p>
+            <p>Klisjé-Kjells Karaokeparty</p>
         </div>
         <div className="innholdsboks">
-          <h3>Lag</h3>
-            <p>Kommer ...</p>
+          <h3>Rebuslag</h3>
+          <div className="lagwrapper">
+            <div className="kolonne">
+              <h4>Lag 1</h4>
+              <p>Sara</p>
+              <p>Sofie G</p>
+              <p>Hege</p>
+              <p>Anna</p>
+              <p>Ine</p>
+              <p>Jonas D</p>
+              <p>Ghers</p>
+              <p>Tobias</p>
+              <p>Andreas</p>
+            </div>
+            <div className="kolonne">
+              <h4>Lag 2</h4>
+              <p>Grete</p>
+              <p>Ane</p>
+              <p>Cecilie</p>
+              <p>Marie B</p>
+              <p>Joachim</p>
+              <p>Sverre</p>
+              <p>Bjørn</p>
+              <p>Morten</p>
+              <br/>
+            </div>
+            <div className="kolonne">
+              <h4>Lag 3</h4>
+              <p>Jorid</p>
+              <p>Camilla</p>
+              <p>Anne</p>
+              <p>Harriet</p>
+              <p>Simen</p>
+              <p>Jens D</p>
+              <p>Abdi</p>
+              <p>Magnus A</p>
+              <br/>
+            </div>
+            <div className="kolonne">
+              <h4>Lag 4</h4>
+              <p>Haga</p>
+              <p>Silje</p>
+              <p>Thea</p>
+              <p>Eirin</p>
+              <p>Jonas N</p>
+              <p>Max</p>
+              <p>Magnus H</p>
+              <p>Sander</p>
+              <br/>
+            </div>
+            <div className="kolonne">
+              <h4>Lag 5</h4>
+              <p>Kine</p>
+              <p>Sofie F</p>
+              <p>Ingvild</p>
+              <p>Nora</p>
+              <p>Julie H</p>
+              <p>Ketil</p>
+              <p>Jens Martin</p>
+              <p>Jonathan</p>
+              <p>Henrik</p>
+            </div>
+          </div>
         </div>
         <div className="polsewrapper">
           {polser}
